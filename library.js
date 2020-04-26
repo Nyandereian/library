bbf7d176edd8b9952791071089fcee3425b4a73c
Nyandereian <nyandereian@gmail.com>
2020-04-26 21:29:16 +0200

    Initialise project

---
+++ library.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.property.info = function () {
+  console.log(title + ' by ' + author + ', ' + pages + ' pages, ' + read '.')
+};
+
+function addBookToLibrary() {
+
+}
